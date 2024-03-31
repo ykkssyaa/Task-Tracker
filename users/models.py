7f7f587949b8e0162df8b36c12912537d673811f
ykkssyaa <mr.ender.03@mail.ru>
2024-03-31 16:30:05 +0300

    custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    TEAMLEAD = 'teamlead'
+    DIRECTOR = 'director'
+    DEVELOPER = 'developer'
+
+    ROLE_CHOICES = [
+        (TEAMLEAD, 'Team Lead'),
+        (DIRECTOR, 'Director'),
+        (DEVELOPER, 'Developer'),
+    ]
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=DEVELOPER)
+
+    def __str__(self):
+        return self.username
